4f260ca61472e43e5591632718e606bd0f71115a
EFTEKHER <eftekherali2000@gmail.com>
2022-06-18 19:48:34 +0600

    c++

---
+++ 1-Array/1-arrayBasic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+ #include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+      
+        
+int main()
+{
+    int a[10]={1,2,3};
+    //print all the first 10 number
+    for(int i=0;i<10;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+        
+return 0;
+}
