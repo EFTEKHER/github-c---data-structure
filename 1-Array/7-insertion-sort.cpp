2a6ac76cbf962f7c93edcaa01905673caed0dfba
EFTEKHER <eftekherali2000@gmail.com>
2022-06-20 20:46:42 +0600

    yes

---
+++ 1-Array/7-insertion-sort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+ #include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+ void insertion_sort(int a[],int n)
+{
+    for(int i=1;i<=n-1;i++)
+    {
+        int e=a[i];
+    //place the current element at 'right' position in the sorted part
+    int j=i-1;
+    while(j>=0 && a[j]>e)
+    {   
+        a[j+1]=a[j];
+        j=j-1;
+
+    }
+    
+    }
+}  
+        
+int main()
+{
+  int a[1000];
+  int n, key;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+  
+  cout<<" printing array "<<endl;
+for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl; 
+
+
+        
+return 0;
+}
