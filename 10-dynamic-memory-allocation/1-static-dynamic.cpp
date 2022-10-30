0a0545b23b7cbdc513f0ea77c4a88a57b7ba0c60
EFTEKHER <eftekherali2000@gmail.com>
2022-10-30 18:50:32 +0600

    yes

--- 10-dynamic-memory-allocation/1-static-dynamic.cpp
+++ 10-dynamic-memory-allocation/1-static-dynamic.cpp
@@ -9,10 +9,28 @@ using namespace std;
         
 int main()
 {
+    //allocation+ dealloction done by compiler
   int a[1000];
   cout<<sizeof(a)<<endl;
+  // a symbol table .here a can not be overwriten , a can only read only memory
 
-  int *aptr=new int(1000);
-  cout<<sizeof(aptr)<<endl;      
+//heap
+ //static part
+  int *aptr=
+  //heap part
+  new int(1000);
+  cout<<sizeof(aptr)<<endl;  
+  int n;
+  cin>>n;
+  //dynamic allocation (on the fly)
+int *arr=new int [n]{0}; //variable arr is created inside the static memory ->it can be over written
+
+  for(int i=0;i<n;i++)
+  {
+    cin>>arr[i];
+    cout<<arr[i]<<endl;
+  }  
+  //free up space
+  delete  []arr;
 return 0;
 }
