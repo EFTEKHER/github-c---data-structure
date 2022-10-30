94ef20bb7282b1927b3b31e171398c08358f0159
EFTEKHER <eftekherali2000@gmail.com>
2022-10-30 19:24:47 +0600

    yup

---
+++ 10-dynamic-memory-allocation/2-dynamic-2d-array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+#define ull unsigned long long int
+#include <ctype.h>
+using namespace std;
+
+int main()
+{
+    int **arr;
+    int r, c;
+    cin >> r >> c;
+
+    // create an array of row heads
+    arr = new int *[r];
+    for (int i = 0; i < r; i++)
+    {
+        arr[i] = new int[c];
+    }
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    return 0;
+}
