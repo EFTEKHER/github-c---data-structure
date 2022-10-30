e01237358f3e2a4660c0ea7b36b3864b5bd703f2
EFTEKHER <eftekherali2000@gmail.com>
2022-10-30 19:41:25 +0600

    y

--- 10-dynamic-memory-allocation/3-return-dynamic-memory.cpp
+++ 10-dynamic-memory-allocation/3-return-dynamic-memory.cpp
@@ -1,27 +1,28 @@
-#include<iostream>
-#include<stdio.h>
-#include<bits/stdc++.h>
-#include<stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
 #define ull unsigned long long int
-#include<ctype.h> 
+#include <ctype.h>
 using namespace std;
- int *fun()
- {
-    int *a=new int [5];
-    a[0]=1;
-    a[1]=2;
-    a[2]=3;
-    a[3]=4;
-    a[4]=5;
-    cout<<a<<endl;
-    cout<<a[0]<<endl;
+int *fun()
+{
+    int *a = new int[5];
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    a[3] = 4;
+    a[4] = 5;
+    cout << a << endl;
+    cout << a[0] << endl;
     return a;
- }     
-        
+}
+
 int main()
 {
-int *b=fun();
-cout<<b<<endl;
-cout<<b[0]<<endl;   
-return 0;
+    int *b = fun();
+    cout << b << endl;
+    cout << b[0] << endl;
+    delete[] b;
+    return 0;
 }
